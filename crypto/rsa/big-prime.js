dc67205b066b9e1df0b99cc4795ef8961de549b4
qq1114282509 <qq1114282509@gmail.com>
2018-02-11 18:47:49 +0800

    大质数

--- crypto/rsa/big-prime.js
+++ crypto/rsa/big-prime.js
@@ -1,8 +1,27 @@
-const BigNumber = require('bignumber.js')
-let bigNumberBinay = '1'
+const Decimal = require('decimal.js')
+const {primes} = require('./prime-table.json')
 
-while (bigNumberBinay.length !== 1024) {
-  bigNumberBinay += Math.floor(Math.random() * 10 >4.5) ? '1': '0'
-  if (bigNumberBinay.length === 1023) bigNumberBinay += '1'
+// 二进制生成512位大数
+const generiteBigNumber = () => {
+  let bigNumberBinay = '0b1'
+  while (bigNumberBinay.length !== 514) {
+    bigNumberBinay += Math.floor(Math.random() * 10 >4.5) ? '1': '0'
+    if (bigNumberBinay.length === 513) bigNumberBinay += '1'
+  }
+  return new Decimal(bigNumberBinay)
 }
-console.log(bigNumberBinay)
+// 素性表格 mod 验证法
+const checkPrimeTable = n => {
+  for ((index) in primes) {
+    if (n.mod(primes[index]) === 0) return false
+  }
+  return true
+}
+
+// 
+
+const generitePrime = () => {
+  const bn = generiteBigNumber()
+  return checkPrimeTable(bn) ? bn.d.join('') : generitePrime()
+}
+console.log(generitePrime())
