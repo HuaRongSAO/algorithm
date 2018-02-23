cc26d4b409da8b08dd35267a7998cc083e6795e6
HuaRongSAO <1114282509@qq.com>
2018-02-23 21:45:06 +0800

    添加求模方法

--- crypto/rsa/miller-rabin.js
+++ crypto/rsa/miller-rabin.js
@@ -30,28 +30,23 @@ class MillerRabin {
     }
     return new Decimal(rn)
   }
-  //快速积取模 a*b%n
-  ModMul (a, b, n) {
-    let ans = 0
-    while (b) {
-      if (b & 1)
-        ans = (ans + a) % n
-      a = (a + a) % n
-      b >>= 1
-    }
-    return ans
+  // 快速积取模 (a * b) mod p = (a mod p * b mod p) mod p
+  modMul (a, b, n) {
+    let firMod = a.mod(n)
+    let senMod = b.mod(n)
+    let product = firMod.mul(senMod)
+    return product.mod(n)
   }
 
-  //快速幂取模 a^b%n
+  //快速幂取模 a ^ b mod p = ((a mod p)^b) mod p
   ModExp (a, b, n) {
-    let ans = 1
-    while (b) {
-      if (b & 1)
-        ans = this.ModMul(ans, a, n)
-      a = this.ModMul(a, a, n)
-      b >>= 1
+    let i = new Decimal(0)
+    let c = a
+    while (!b.equals(i)) {
+      c = this.modMul(c, a, n)
+      i = i.plus(1)  
     }
-    return ans
+    return c
   }
 
   millerRabinLoop (s) {
@@ -59,34 +54,30 @@ class MillerRabin {
     // n-1 = u*2^t
     let t = 0
     let u = bn.sub(1)
-    while (u.mod(2).d[0] === 0) {
+    while (u.mod(2).equals(0)) {
       u = u.div(2).floor()
       t++
     }
-    console.log(u.d[0], t)
+    console.log('u,t',u.d,t)
     for (let i = 0; i < s; i++) {
       let a = this.getRandom()
       while (a.lt(1) || a.gt(bn)) {
         a = this.getRandom()
       }
       // x=(a^u) mod n
-      console.log('u,t', u.d, t)
-      // console.log('a', a.pow(u).d)
-      console.log('toBinary', a.toBinary)
-      console.log(this.ModExp(a.toBinary, '0b011', bn.toBinary))
-
-      // let x = new Decimal(a.pow(u).d.join('')).mod(bn)
-      // console.log('a.pow(u)', a.pow(u).d)
-      // console.log(x)
+      let x =  this.ModExp(a, u, bn)
+      console.log('a' + a.d)
+      console.log('x' + x.d)
       for (let j = 0; j < t; j++) {
         // console.log('x:' + j + ':', x.d)
-        let y = new Decimal(new Decimal(x).pow(2).d.join('')).mod(bn)
+        let y = x.pow(2).mod(bn)
         console.log('y:' + j + ':', y.d)
         if (y.equals(1) && !x.equals(1) && !x.equals(bn.sub(1))) return false
         x = y
       }
-      //这里的时候x=a^(n-1)，根据费马小定理，x!=1的话，肯定不是素数了
-      console.log('x!=1的话', x.equals(1))
+      // 这里的时候x=a^(n-1)，根据费马小定理，x!=1的话，肯定不是素数了
+      console.log('x.equals(1)' + x.equals(1))
+      if (x.equals(1)) return true
       if (!x.equals(1)) return false
     }
     return true
@@ -94,5 +85,5 @@ class MillerRabin {
 
 }
 
-const mr = new MillerRabin('33478071698956898786044169848212690817704794983713768568912431388982883793878002287614711652531743087737814467999489')
+const mr = new MillerRabin('13')
 console.log(mr.isPrime())
